9b7e978af20921fcabd4fa8810b4e204e9be022b
Tuwuh S <tuwuhs@gmail.com>
2020-09-16 22:04:53 -0400

    Hello Pose3

--- main.cpp
+++ main.cpp
@@ -2,7 +2,14 @@
 #include <iostream>
 
 #include <gtsam/config.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/geometry/Rot3.h>
+
+using namespace gtsam;
 
 int main(int, char**) {
-    std::cout << "Hello, world! GTSAM version:" << GTSAM_VERSION_STRING << std::endl;
+    std::cout << "Hello, world! GTSAM version: " << GTSAM_VERSION_STRING << std::endl;
+
+    Pose3 pose(Rot3::Rodrigues(0.1, 0.2, 0.3), Point3(1.0, 2.0, 3.0));
+    std::cout << pose << std::endl;
 }
